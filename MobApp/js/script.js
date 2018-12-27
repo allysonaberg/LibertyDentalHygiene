7239fd541f7b5966f4313d647318c2031d0261f5
Allyson Aberg <allya7897@gmail.com>
2018-12-27 10:55:08 -0500

    note to self: no horizontal scroll on this commit

--- MobApp/js/script.js
+++ MobApp/js/script.js
@@ -5,7 +5,7 @@ Version : 1.0
 Author  : Surjith S M
 URI     : https://surjithctly.in/
 
-Copyright © All rights Reserved 
+Copyright © All rights Reserved
 
 */
 
@@ -15,35 +15,35 @@ $(function() {
     /*-----------------------------------
      * FIXED  MENU - HEADER
      *-----------------------------------*/
-    function menuscroll() {
-        var $navmenu = $('.nav-menu');
-        if ($(window).scrollTop() > 50) {
-            $navmenu.addClass('is-scrolling');
-        } else {
-            $navmenu.removeClass("is-scrolling");
-        }
-    }
-    menuscroll();
-    $(window).on('scroll', function() {
-        menuscroll();
-    });
+    // function menuscroll() {
+    //     var $navmenu = $('.nav-menu');
+    //     if ($(window).scrollTop() > 50) {
+    //         $navmenu.addClass('is-scrolling');
+    //     } else {
+    //         $navmenu.removeClass("is-scrolling");
+    //     }
+    // }
+    // menuscroll();
+    // $(window).on('scroll', function() {
+    //     menuscroll();
+    // });
     /*-----------------------------------
      * NAVBAR CLOSE ON CLICK
      *-----------------------------------*/
 
-    $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
-        $('.navbar-collapse').collapse('hide');
-    });
-    /* 
-     * NAVBAR TOGGLE BG
-     *-----------------*/
-    var siteNav = $('#navbar');
-    siteNav.on('show.bs.collapse', function(e) {
-        $(this).parents('.nav-menu').addClass('menu-is-open');
-    })
-    siteNav.on('hide.bs.collapse', function(e) {
-        $(this).parents('.nav-menu').removeClass('menu-is-open');
-    })
+    // $('.navbar-nav > li:not(.dropdown) > a').on('click', function() {
+    //     $('.navbar-collapse').collapse('hide');
+    // });
+    // /*
+    //  * NAVBAR TOGGLE BG
+    //  *-----------------*/
+    // var siteNav = $('#navbar');
+    // siteNav.on('show.bs.collapse', function(e) {
+    //     $(this).parents('.nav-menu').addClass('menu-is-open');
+    // })
+    // siteNav.on('hide.bs.collapse', function(e) {
+    //     $(this).parents('.nav-menu').removeClass('menu-is-open');
+    // })
 
     /*-----------------------------------
      * ONE PAGE SCROLLING
@@ -76,37 +76,5 @@ $(function() {
             }
         }
     });
-    /*-----------------------------------
-     * OWL CAROUSEL
-     *-----------------------------------*/
-    var $testimonialsDiv = $('.testimonials');
-    if ($testimonialsDiv.length && $.fn.owlCarousel) {
-        $testimonialsDiv.owlCarousel({
-            items: 1,
-            nav: true,
-            dots: false,
-            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>']
-        });
-    }
-
-    var $galleryDiv = $('.img-gallery');
-    if ($galleryDiv.length && $.fn.owlCarousel) {
-        $galleryDiv.owlCarousel({
-            nav: false,
-            center: true,
-            loop: true,
-            autoplay: true,
-            dots: true,
-            navText: ['<span class="ti-arrow-left"></span>', '<span class="ti-arrow-right"></span>'],
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 3
-                }
-            }
-        });
-    }
 
 }); /* End Fn */
